32fc346364e8684ea9749c0b59590a9ec82cc5b8
Veles2006 <anhphiax147x@gmail.com>
2025-06-14 01:50:26 +0800

    latest-posts, brands và instagram đã được cập nhật

--- src/pages/Home/Instagram/index.jsx
+++ src/pages/Home/Instagram/index.jsx
@@ -1,27 +1,36 @@
-import classNames from "classnames/bind";
-import styles from './Instagram.module.scss'
-import { Link } from "react-router-dom";
+import classNames from 'classnames/bind';
+import styles from './Instagram.module.scss';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
-function Instagram({ images= [] }) {
-    return <div className={cx('instagram-page', 'grid', 'wide')}>
-        <div className={cx('instagram-title')}>
-            <h3>Instagram #shoplite</h3>
-        </div>
-        <div className={cx('row')}>
-            {images.map((image, idx) => (
-                <div className={cx('l-2')} key={idx}>
-                <figure className={cx('instagram-item')}>
-                    <Link to={'#'}>
-                        <div className={cx('instagram')}></div>
-                        <img src={image} alt="" />
-                    </Link>
-                </figure>
+function Instagram({ images = [] }) {
+    return (
+        <div className={cx('instagram-page')}>
+            <div className={cx('grid', 'wide')}>
+                <div className={cx('instagram-title')}>
+                    <h3>Instagram #shoplite</h3>
+                </div>
+                <div className={cx('row')}>
+                    {images.map((image, idx) => (
+                        <div className={cx('col', 'l-2')} key={idx}>
+                            <figure className={cx('instagram-item')}>
+                                <Link to={'#'}>
+                                    
+                                    <img src={image} alt="" />
+                                </Link>
+                                <div className={cx('instagram')}>
+                                        <FontAwesomeIcon icon={faInstagram} />
+                                    </div>
+                            </figure>
+                        </div>
+                    ))}
+                </div>
             </div>
-            ))}
         </div>
-    </div>;
+    );
 }
 
 export default Instagram;
--- src/pages/Home/LatestpPosts/index.jsx
+++ src/pages/Home/LatestpPosts/index.jsx
@@ -7,28 +7,33 @@ const cx = classNames.bind(styles);
 function LatestpPosts({ latestpPosts = [] }) {
     return (
         <div className={cx('latest-posts')}>
-            <div className={cx('latest-posts-title')}>
-                <h3>Latest posts</h3>
-            </div>
-            <div className={cx('latest-posts-content')}>
-                <div className={cx('grid', 'wide')}>
-                    <div className={cx('row')}>
-                        {latestpPosts.map((post, idx) => (
-                            <div className={cx('post', 'l-3')}>
-                                <div className={cx('post-img')}>
-                                    <img src={post.img} />
+            <div className={cx('grid', 'wide')}>
+                <div className={cx('latest-posts-title')}>
+                    <h3>Latest posts</h3>
+                </div>
+                <div className={cx('latest-posts-content')}>
+                    <div className={cx('grid', 'wide')}>
+                        <div className={cx('row')}>
+                            {latestpPosts.map((post, idx) => (
+                                <div className={cx('col', 'l-3')}>
+                                    <div className={cx('post')}>
+                                        <div className={cx('post-btn')}>{post.flag}</div>
+                                        <div className={cx('post-img')}>
+                                            <img src={post.image} />
+                                        </div>
+                                        <div className="post-content">
+                                            <h4 className={cx('post-content__title')}>
+                                                <Link to={'#'}>{post.title}</Link>
+                                            </h4>
+                                            <p className={cx('post-content__paragraph')}>
+                                                {post.paragraph}
+                                            </p>
+                                            <Link to={'#'} className={cx('post-content__read-more')}>Read More</Link>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="post-content">
-                                    <h4 className="post-content__title">
-                                        <Link to={'#'}>{post.title}</Link>
-                                    </h4>
-                                    <p className={cx('post-content__paragraph')}>
-                                        {post.paragraph}
-                                    </p>
-                                    <Link to={'#'} className={cx('post-content__read-more')}>Read More</Link>
-                                </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
